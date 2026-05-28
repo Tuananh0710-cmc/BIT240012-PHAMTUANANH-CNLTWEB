3d90ca37e8f7910469aefbbc93e653740488820c
Tuan Anh <panhhh0710@gmail.com>
2026-05-28 21:54:28 +0700

    bài3

---
+++ bài 3/BookManagement/BookManagement/Controllers/BookController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookManagement.Controllers
+{
+    public class BookController : Controller
+    {
+        // Danh sách sách
+        public IActionResult Index()
+        {
+            var books = new List<dynamic>()
+            {
+                new { Id = 1, Name = "Clean Code", Price = 20 },
+                new { Id = 2, Name = "ASP.NET MVC", Price = 15 },
+                new { Id = 3, Name = "Design Pattern", Price = 25 }
+            };
+
+            return View(books);
+        }
+    }
+}
