3fe141522215242f4e6e18a212c6e059c8ee828f
Tuan Anh <panhhh0710@gmail.com>
2026-06-01 07:26:49 +0700

    baitapbuoi4

---
+++ baitapbuoi4/bai6/BookManagement/Controllers/BookController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BookManagement.Models;
+
+namespace BookManagement.Controllers
+{
+    public class BookController : Controller
+    {
+        // Danh sách sách
+        public IActionResult Index()
+        {
+            var books = new List<dynamic>()
+            {
+                new { Id = 1, Name = "Clean Code", Price = 20 },
+                new { Id = 2, Name = "ASP.NET MVC", Price = 15 },
+                new { Id = 3, Name = "Design Pattern", Price = 25 }
+            };
+
+            return View(books);
+        }
+
+        // Chi tiết sách
+        public IActionResult Detail(int id)
+        {
+            var book = new
+            {
+                Id = id,
+                Name = "Clean Code",
+                Price = 20
+            };
+
+            return View(book);
+        }
+
+        // Hiển thị form thêm sách
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        // Xử lý thêm sách
+        [HttpPost]
+        public IActionResult Add(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Message = "Thêm sách thành công!";
+            }
+
+            return View(book);
+        }
+    }
+}
---
+++ baitapbuoi4/bai6/BookManagement/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookManagement.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Không được để trống")]
+        public string Name { get; set; }
+
+        [Range(1, 1000, ErrorMessage = "Giá phải lớn hơn 0")]
+        public int Price { get; set; }
+    }
+}
